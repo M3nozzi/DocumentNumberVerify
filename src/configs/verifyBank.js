9a34d0d9a814e58a6815695ff710b866ff119553
Fabio Menozzi <f.menozzi@yahoo.com>
2020-08-10 17:49:28 -0300

    building to get due date

--- src/configs/verifyBank.js
+++ src/configs/verifyBank.js
@@ -6,7 +6,52 @@ const verifyBank = {
 
         let dvTest = 0;
         let mult = 2;
-  }
+
+        for (let i = field.length - 1; i >= 0; i--){
+            dvTest += Number(field[i]) * mult;
+            
+            if (modDAC == 10) {
+                if ((Number(field[i]) * mult) > 9) {
+                    dvTest -= 9;
+                };
+                
+                if (mult == 2) {
+                    mult--;
+                } else {
+                    mult++;
+                };
+            };
+
+            if (modDAC == 11) {
+                if (mult == 9) {
+                    mult = 2;
+                } else {
+                    mult++;
+                };
+            };
+
+        };
+
+        if (modDAC == 11) {
+            dvTest = devTest % 11;
+
+            if (dvTest == 1) {
+                dvTest = 0;
+            } else if (dvTest == 10) {
+                dvTest = 1;
+            };
+        };
+
+        if (dv == dvTest) {
+            return true;
+        } else {
+            return false;
+        };
+
+    },
+    
+
+    
 
 };
 
